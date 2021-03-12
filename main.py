eb3451a4f5c77c7195531e84311b3d87a86f8b7a
Geomodulation <chicagoswiss666@gmail.com>
2021-03-12 12:49:35 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+name = input ("Enter name: ") #these first two are imputs to define
+age = input ("Enter age: ")
+def truncate(n,decimals=0):
+    multiplier = 10** decimals
+    return int(n * multiplier) / multiplier
+
+
+truncate (43,-1)
+
+
+
+
+
+
+if age1<age= return age1+10    #these next two lines are to make sure the age is rounded up, so age1 is rounded up
+else= return age1
+age1-age=numx                   #for this line the up rounded age1 minuse current age= time till next decade
+
+
